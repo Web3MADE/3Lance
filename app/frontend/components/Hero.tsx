9850bb6645a1be3440de39810c95c64a3f626a7f
Hakeem <mwo248@yahoo.com>
2024-03-31 11:43:54 +0100

    hero image

--- app/frontend/components/Hero.tsx
+++ app/frontend/components/Hero.tsx
@@ -1,13 +1,20 @@
 "use client";
 import { Box, Container } from "@mui/material";
+import Image from "next/image";
 
 export default function Hero() {
   return (
-    <Container>
+    <Container sx={{ display: "flex" }}>
       <Box>
         <h1>3Lance</h1>
         <p>Find the perfect freelancer for your project.</p>
       </Box>
+      <Image
+        src="/web3freelancechillin.jpeg"
+        alt="freelancer chillin"
+        width={500}
+        height={300}
+      />
     </Container>
   );
 }
