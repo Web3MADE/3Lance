ee2cb578b142f7cdc4cac3cfe76e2053fbd52f27
Hakeem <mwo248@yahoo.com>
2024-03-31 10:21:46 +0100

    basic home setup

---
+++ app/frontend/Home.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Box } from "@mui/material";
+
+// id used to differentiate between different home pages (client/freelancer)?
+export default function Home() {
+  return (
+    <main>
+      <Box></Box>
+    </main>
+  );
+}
