302a456ff1c99a61a036ca37619d5d1ce756959d
Hakeem <mwo248@yahoo.com>
2024-04-01 13:03:55 +0100

    privy provider

---
+++ app/frontend/providers/PrivyProvider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { PrivyProvider as Privy } from "@privy-io/react-auth";
+
+export default function PrivyProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  if (!process.env.NEXT_PUBLIC_PRIVY_APP_ID) {
+    console.error("app ID invalid");
+    return;
+  }
+  return (
+    <Privy
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID}
+      config={{
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: "light",
+          accentColor: "#676FFF",
+          logo: "https://your-logo-url",
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: "users-without-wallets",
+        },
+      }}
+    >
+      {children}
+    </Privy>
+  );
+}
