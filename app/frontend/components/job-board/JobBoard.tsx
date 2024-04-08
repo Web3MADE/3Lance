32374efe3df36bfc294c39e63e1e32790ebbc3b3
Hakeem <mwo248@yahoo.com>
2024-04-08 09:37:21 +0100

    remove fragment

--- app/frontend/components/job-board/JobBoard.tsx
+++ app/frontend/components/job-board/JobBoard.tsx
@@ -49,54 +49,54 @@ export default function JobBoard() {
     );
   }, [getSignature, isWalletReady, wallet.address]);
 
+  {
+    /**@dev Container is ideal for page level container due to default padding */
+  }
   return (
-    <>
-      {/**@dev Container is ideal for page level container due to default padding */}
-      <Container
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        justifyContent: "center",
+        gap: "1rem",
+      }}
+    >
+      <Box sx={{ width: "50%" }}>
+        <Card>
+          <CardHeader title="Need a Freelancer?" />
+          <CardContent
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <CardActions
+              sx={{ display: "flex", justifyContent: "flex-start", pl: 0 }}
+            >
+              <Button sx={{ padding: 0 }}>Post a Job</Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+      </Box>
+
+      <Box>
+        <Typography variant="h4">Job Postings</Typography>
+      </Box>
+      <Box
         sx={{
           display: "flex",
-          flexDirection: "column",
-          height: "100vh",
-          justifyContent: "center",
           gap: "1rem",
+          alignItems: "center",
+          mt: 2,
         }}
       >
-        <Box sx={{ width: "50%" }}>
-          <Card>
-            <CardHeader title="Need a Freelancer?" />
-            <CardContent
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-              }}
-            >
-              <CardActions
-                sx={{ display: "flex", justifyContent: "flex-start", pl: 0 }}
-              >
-                <Button sx={{ padding: 0 }}>Post a Job</Button>
-              </CardActions>
-            </CardContent>
-          </Card>
-        </Box>
-
-        <Box>
-          <Typography variant="h4">Job Postings</Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            gap: "1rem",
-            alignItems: "center",
-            mt: 2,
-          }}
-        >
-          <JobCard onClick={handleAttestByDelegation} />
-          <JobCard onClick={handleAttestByDelegation} />
-          <JobCard onClick={handleAttestByDelegation} />
-          <JobCard onClick={handleAttestByDelegation} />
-        </Box>
-      </Container>
-    </>
+        <JobCard onClick={handleAttestByDelegation} />
+        <JobCard onClick={handleAttestByDelegation} />
+        <JobCard onClick={handleAttestByDelegation} />
+        <JobCard onClick={handleAttestByDelegation} />
+      </Box>
+    </Container>
   );
 }
