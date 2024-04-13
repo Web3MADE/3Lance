691f79ff5719fa3b538e0b853c216a6b678681ec
Hakeem <mwo248@yahoo.com>
2024-04-13 11:39:00 +0100

    unusued code

--- app/utils/Attest.ts
+++ app/utils/Attest.ts
@@ -1,47 +1,47 @@
-// TODO: Can I use Privy to store metadata (attestations) or do I need AWS DynamoDB?
+// // TODO: Can I use Privy to store metadata (attestations) or do I need AWS DynamoDB?
 
-import {
-  REPUTATION_SCHEMMA_UID,
-  getEAS,
-  projectSchemaEncoder,
-  reputationSchemaEncoder,
-} from "../config/EAS";
+// import {
+//   REPUTATION_SCHEMMA_UID,
+//   getEAS,
+//   projectSchemaEncoder,
+//   reputationSchemaEncoder,
+// } from "../config/EAS";
 
-// TODO:
-// On Successful User sign up, we will create a new reputation for the user
-// confirm this works in API from admin wallet
-// THEN integrate sponsored transactions = we need the user to DO the attestation from their private key, but with our gas
-export async function attestNewReputation(
-  recipient: string,
-  name: string,
-  email: string
-) {
-  const { eas } = getEAS();
-  const encodedData = reputationSchemaEncoder.encodeData([
-    { name: "name", value: name, type: "string" },
-    { name: "email", value: email, type: "string" },
-    { name: "reputation", value: 1, type: "uint8" },
-  ]);
+// // TODO:
+// // On Successful User sign up, we will create a new reputation for the user
+// // confirm this works in API from admin wallet
+// // THEN integrate sponsored transactions = we need the user to DO the attestation from their private key, but with our gas
+// export async function attestNewReputation(
+//   recipient: string,
+//   name: string,
+//   email: string
+// ) {
+//   const { eas } = getEAS();
+//   const encodedData = reputationSchemaEncoder.encodeData([
+//     { name: "name", value: name, type: "string" },
+//     { name: "email", value: email, type: "string" },
+//     { name: "reputation", value: 1, type: "uint8" },
+//   ]);
 
-  const tx = await eas.attest({
-    schema: REPUTATION_SCHEMMA_UID,
-    data: {
-      recipient,
-      data: encodedData,
-    },
-  });
+//   const tx = await eas.attest({
+//     schema: REPUTATION_SCHEMMA_UID,
+//     data: {
+//       recipient,
+//       data: encodedData,
+//     },
+//   });
 
-  const newAttestationUID = await tx.wait();
-  console.log("new attestation UID: ", newAttestationUID);
-  return newAttestationUID;
-}
+//   const newAttestationUID = await tx.wait();
+//   console.log("new attestation UID: ", newAttestationUID);
+//   return newAttestationUID;
+// }
 
-export const encodedProjectData = projectSchemaEncoder.encodeData([
-  { name: "projectID", value: "projectID", type: "bytes32" },
-  {
-    name: "Freelancer",
-    value: "0x6116ABf3445d8744bF78c8c7B322cD5A91613fbA",
-    type: "address",
-  },
-  { name: "isCompleted", value: true, type: "bool" },
-]);
+// export const encodedProjectData = projectSchemaEncoder.encodeData([
+//   { name: "projectID", value: "projectID", type: "bytes32" },
+//   {
+//     name: "Freelancer",
+//     value: "0x6116ABf3445d8744bF78c8c7B322cD5A91613fbA",
+//     type: "address",
+//   },
+//   { name: "isCompleted", value: true, type: "bool" },
+// ]);
